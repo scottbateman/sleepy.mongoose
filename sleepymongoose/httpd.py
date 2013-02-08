9c8235dfa6fb08331eac9f6e5891955285a8d1b1
scottbateman <sbateman@upei.ca>
2013-02-08 12:16:17 -0400

    Revert "added unique query"
    This reverts commit cf64e697ebdd580322df7761bfb2b31a9160737e.

--- sleepymongoose/httpd.py
+++ sleepymongoose/httpd.py
@@ -284,37 +284,6 @@ def main():
         sys.exit(2)
 
     MongoHTTPRequest.serve_forever(27080)
-'''
 if __name__ == "__main__":
     main()
-'''
-if __name__ == "__main__":
-    # do the UNIX double-fork magic, see Stevens' "Advanced 
-    # Programming in the UNIX Environment" for details (ISBN 0201563177)
-    try: 
-        pid = os.fork() 
-        if pid > 0:
-            # exit first parent
-            sys.exit(0) 
-    except OSError, e: 
-        print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1)
 
-    # decouple from parent environment
-    os.chdir("/") 
-    os.setsid() 
-    os.umask(0) 
-
-    # do second fork
-    try: 
-        pid = os.fork() 
-        if pid > 0:
-            # exit from second parent, print eventual PID before
-            print "Daemon PID %d" % pid 
-            sys.exit(0) 
-    except OSError, e: 
-        print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1) 
-
-    # start the daemon main loop
-    main()
